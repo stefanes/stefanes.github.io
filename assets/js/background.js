08cbbaa917bb1914004f4eaf5891eb6af695f470
Stefan Eskelid <stefan.eskelid@aveva.com>
2024-10-07 13:04:30 +0200

    Move script to file

---
+++ assets/js/background.js
@@ -0,0 +1,27 @@
+const images = [
+  "alp-village.jpg",
+  "autumn-lake.jpg",
+  "autumn-trail.jpg",
+  "beach.jpg",
+  "canyon.jpg",
+  "coffee.jpg",
+  "cove.jpg",
+  "curves.jpg",
+  "fog.jpg",
+  "green-canyon.jpg",
+  "icy-canal.jpg",
+  "mountains.jpg",
+  "pier.jpg",
+  "railway.jpg",
+  "sunset.jpg",
+  "tree-on-hill.jpg",
+  "winter-lake.jpg",
+];
+function changeBackground() {
+  const randomIndex = Math.floor(Math.random() * images.length);
+  document.body.style.backgroundImage =
+    'linear-gradient(var(--linear-gradient), var(--linear-gradient)), url("assets/img/' +
+    images[randomIndex] +
+    '")';
+}
+changeBackground();
